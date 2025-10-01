50e9ae8fc864033cfd00f2e2d5d4d0f5a7bd0cef
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-10-01 15:52:39 +0300

    add (02-mongodb)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nodejs-hw
+
+<!-- stomaludmila537_db_user -->
+<!-- WrIk0aSkXnTSOtv0 -->
--- src/server.js
+++ src/server.js
@@ -2,41 +2,31 @@ import express from 'express';
 import 'dotenv/config.js';
 import cors from 'cors';
 import pino from 'pino-http';
+import helmet from 'helmet';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT ?? 3030;
 
+app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(pino());
+app.use(helmet());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-//тестовий маршрут для імітації виникнення помилки
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+app.use(notesRoutes);
 
 //Обробка неіснуючих маршрутів
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
+app.use(notFoundHandler);
 
 //Обробка помилок
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
-  });
-});
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

b275626e60ccb4e17111e47daeec8cd3621440fb
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-10-13 20:55:35 +0300

    add index

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -15,10 +15,11 @@ export const getAllNotes = async (req, res) => {
   }
   if (search) {
     notesQuery.where({
-      $or: [
-        { title: { $regex: search, $options: 'i' } },
-        { content: { $regex: search, $options: 'i' } },
-      ],
+      $text: { $search: search },
+      // $or: [
+      //   { title: { $regex: search, $options: 'i' } },
+      //   { content: { $regex: search, $options: 'i' } },
+      // ],
     });
   }
 
--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import { Note } from '../models/note.js';
 
 export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGODB_URL;
     await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
+
+    await Note.syncIndexes();
   } catch (error) {
     console.log('❌ Filed to connect to MongoDB:', error.message);
     process.exit(1);

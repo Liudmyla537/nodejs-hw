2efd38ce29f8cdebdf7c4ea5652a8b15b1c2895b
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-10-16 07:01:31 +0300

    fix notesController-getNoteById

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -4,7 +4,6 @@ export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
-    username: Joi.string().trim(),
   }),
 };
 
